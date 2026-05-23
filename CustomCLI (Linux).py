fc7901310b87b6597b8dc76739edc171253d1fcb
aandrassignment <aandrassignment@gmail.com>
2026-05-23 22:30:47 +0700

    actual code from VM (tested)

--- CustomCLI (Linux).py
+++ CustomCLI (Linux).py
@@ -5,95 +5,93 @@ from socket import gethostname
 cli_loop = True
 
 def tokenizer(command, arguments):
-  commands=[]
-  current_command=[]
-  full_command = [command] + arguments
-  for item in full_command:
-    if item == "|":
-      commands.append(current_command)
-      current_command=[]
-    else:
-      current_command.append(item)
+    commands=[]
+    current_command=[]
+    full_command = [command] + arguments
+    for item in full_command:
+        if item == "|":
+            commands.append(current_command)
+            current_command=[]
+        else:
+            current_command.append(item)
     commands.append(current_command)
+    # print(commands)
     return commands
-  
+
 def externals(command, arguments):
-  full_command = [command] + arguments
-  pid = os.fork()
-  if pid == 0:
-    os.execvp(command, full_command)
-  else:
-    os.wait()
+    full_command = [command] + arguments
+    pid = os.fork()
+    if pid == 0 :
+        os.execvp(command, full_command)
+    else:
+        os.wait()
 
 def advanced(command, arguments):
-  commands=tokenizer(command, arguments)
-  prev_read=None
-  pids=[]
-  for i, index in enumerate(commands):
-    if i != len(commands)-1:
-      r,w = os.pipe()
+    commands = tokenizer(command, arguments)
+    prev_read=None
+    pids=[]
+    for i, index in enumerate(commands):
 
-    pid=os.fork()
-    #logic : if not last write, if not first read
-    if pid == 0:
-      if prev_read != None:
-        os.dup2(prev_read, 0)
-        os.close(prev_read)
+        if i != len(commands)-1:
+            r,w = os.pipe()
+        
+        pid=os.fork()
+        # Logic : if not last write, if not first read
+        if pid==0:
+            if prev_read != None:               
+                os.dup2(prev_read, 0)
+                os.close(prev_read)             
 
-      if i<len(commands)-1:
-        os.dup2(w,1)
-        os.close(r)
-        os.close(w)
+            if i<len(commands)-1:
+                os.dup2(w, 1)
+                os.close(r)
+                os.close(w)
+            
+            os.execvp(index[0],index)
+        else:
+            pids.append(pid)
+            if prev_read != None:
+                os.close(prev_read)
+            if i<len(commands)-1:
+                os.close(w)
+                prev_read  = r
 
-      os.execv(index[0],index)
-    else:
-      pids.append(pid)
-      if prev_read != None:
-        os.close(prev_read)
-      if i<len(commands)-1:
-        os.close(w)
-        prev_read=r
-    
     for pid in pids:
-      os.waitpid(pid, 0)
+        os.waitpid(pid, 0)
 
-  def getCwd():
+def getCwd():
     print(f"Current Directory : {os.getcwd()}")
-  
-  def changeDirectory(arguments):
+
+def changeDirectory(arguments) :
     if arguments:
-      try:
-        os.chdir(arguments[0])
-        getCwd()
-      except:
-        print("Invalid Path")
-  
-  def getHelp() :
+        try:
+            os.chdir(arguments[0])
+            getCwd()
+        except :
+            print("Invalid Path")
+
+def getHelp() :
     print("Available Commands : ")
-  
-  while cli_loop :
-    cliprompt = f"\033[36m\033[1m{gethostname()}@CustomCLI\033[0m\033[33m\033[1m:~{os.getcwd()}$\033[0m"
+
+while cli_loop :
+    cliprompt = f"\033[36m\033[1m{gethostname()}@CustomCLI\033[0m\033[33m\033[1m:~{os.getcwd()}$\033[0m "
     user_input = input(cliprompt)
     user_input = split(user_input)
     if not user_input:
-      continue
+        continue
 
     command = user_input[0]
     arguments = user_input[1:]
 
     match command :
-      case ".exit":
-        cli_loop=False
-
-      case "getcwd":
-        getCwd()
-
-      case "cd" :
-        changeDirectory(arguments)
-
-      case "help" | "?":
-        getHelp()
-
-      case _:
-        # externals(command, arguments)
-        advanced(command, arguments)
+        case ".exit":
+            cli_loop = False
+        case "getcwd":
+            getCwd()
+        case "cd":
+            changeDirectory(arguments)
+        case "help" | "?":
+            getHelp()
+        case _:
+            # externals(command, arguments)
+            advanced(command, arguments)
